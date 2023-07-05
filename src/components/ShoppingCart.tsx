c06e34f3b4fe1f68f93d0ceed7d611c63f645486
Tharindu-Dasantha <t.dasanthaedirisinghe@gmail.com>
2023-07-05 11:38:52 +0530

    almost done
    home and about still have to impliment

--- src/components/ShoppingCart.tsx
+++ src/components/ShoppingCart.tsx
@@ -1,6 +1,8 @@
 import { Offcanvas, Stack } from "react-bootstrap";
 import { useShoppingCart } from "../context/ShoppingCartContext";
 import { CartItem } from "./Cartitem";
+import storeItems from "../data/items.json";
+import { formatCurrency } from "../utilities/formatCurrency";
 
 type ShoppingCartProps = {
   isOpen: boolean;
@@ -15,9 +17,19 @@ export function ShoppingCart({ isOpen }: ShoppingCartProps) {
       </Offcanvas.Header>
       <Offcanvas.Body>
         <Stack gap={3}>
-          {cartItems.map((item:any) => (
+          {cartItems.map((item: any) => (
             <CartItem key={item.id} {...item} />
           ))}
+
+          <div className="ms-auto fw-bold fs-5">
+            Total{" "}
+            {formatCurrency(
+              cartItems.reduce((total: number, cartItem: { id: any; quantity: number; }) => {
+                const item = storeItems.find(i => i.id === cartItem.id);
+                return total + (item?.price || 0) * cartItem.quantity
+              }, 0)
+            )}
+          </div>
         </Stack>
       </Offcanvas.Body>
     </Offcanvas>
